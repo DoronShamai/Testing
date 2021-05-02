6c4f18328d1716c9137deca23886bfe8fc10fd3c
DoronShamai <doron@laylinetech.com>
2021-05-02 10:29:46 +0300

    [ADD] email align

---
+++ Testing/emails_align/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Email Align',
+    'version': '1.0.1',
+    'category': 'Sales/CRM',
+    'sequence': 50,
+    'summary': 'Email Align',
+    'description': "",
+    'depends': [
+        'base', 'stock','contacts','sale_management'
+    ],
+    'data': [
+
+    #    'views/stock_picking_batch_views.xml',
+       'security/ir.model.access.csv',
+    #    'report/report_delivery_slot_notes.xml',
+
+       
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
